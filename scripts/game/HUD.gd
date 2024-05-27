e8fb5f19778381834c689b610d589e7615e436a5
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2024-05-27 16:53:44 -0400

    player has 3 revives and then gets game over screen

--- scripts/game/HUD.gd
+++ scripts/game/HUD.gd
@@ -1,10 +1,40 @@
 extends CanvasLayer
 
 @onready var gem_count = $GemCount
+@onready var icon = $Life/Icon
+@onready var icon_1 = $Life/Icon1
+@onready var icon_2 = $Life/Icon2
 
 func _ready():
+	# Set gem counter to number in save file
 	gem_count.text = str(SaveManager.gems)
+	# Listen for gem collection event
 	Signals.gem_collected.connect(self._on_gem_collected)
+
+	# Listen for death event
+	Signals.lost_life.connect(self._on_lost_life)
+	
+	match SaveManager.lives:
+		3:
+			icon.visible = true
+			icon_1.visible = true
+			icon_2.visible = true
+		2:
+			icon.visible = true
+			icon_1.visible = true
+			icon_2.visible = false
+		1:
+			icon.visible = true
+			icon_1.visible = false
+			icon_2.visible = false
+		_:
+			icon.visible = false
+			icon_1.visible = false
+			icon_2.visible = false
+			
+func _on_lost_life():
+	# Update life count
+	SaveManager.lives -= 1
 
 func _on_gem_collected():
 	SaveManager.gems += 1
--- signals.gd
+++ signals.gd
@@ -1,3 +1,4 @@
 extends Node
 
 signal gem_collected
+signal lost_life
