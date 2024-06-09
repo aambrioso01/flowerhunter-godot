8009f8e9ff3bc04991c7b4ae3a60e3f0f2dd7715
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2024-06-08 20:46:19 -0400

    player projectiles collide with walls and enemies which extend CharacterBody2D

--- scripts/enemies/chomper.gd
+++ scripts/enemies/chomper.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 const SPEED = 60
 
@@ -8,8 +8,6 @@ var direction = 1
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if ray_cast_right.is_colliding():
 		direction = -1
@@ -17,5 +15,5 @@ func _process(delta):
 	if ray_cast_left.is_colliding():
 		direction = 1
 		animated_sprite.flip_h = true
-	
+
 	position.x += direction * SPEED * delta
--- scripts/projectiles/spitball.gd
+++ scripts/projectiles/spitball.gd
@@ -1,13 +1,11 @@
-extends Node2D
-
-#extends Area2D
+extends Area2D
 
 var speed = -10000
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
 
-func _on_Bullet_body_entered(body):
-	if body.is_in_group("mobs"):
+func _on_body_entered(body):
+	if body.is_in_group("enemy"):
 		body.queue_free()
 	queue_free()
